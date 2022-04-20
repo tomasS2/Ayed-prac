a3bb8273e7f9017e46d48974a96106f9092e0cdb
Tomas <101843390+tomasS2@users.noreply.github.com>
2022-04-20 12:06:41 -0300

    Add files via upload

---
+++ tp3_ab.zip_expanded/ejercicio4/RedBinariaLlena.java
@@ -0,0 +1,41 @@
+package tp03.ejercicio4;
+
+import tp03.ejercicio1.ArbolBinario;
+
+public class RedBinariaLlena {
+	private ArbolBinario <Integer> arbol;
+	private int max = -1;
+	
+	
+	public RedBinariaLlena (ArbolBinario<Integer> arbol) {
+		this.arbol = arbol;
+		
+	}
+	
+	
+	public int retardoReenvio(ArbolBinario<Integer> arbol) {
+		Integer cont = 0;
+		calcRetardo (cont,arbol);
+		return max;
+	}
+	
+	
+	private void calcRetardo (Integer cont, ArbolBinario <Integer> a ) {
+		cont = cont + a.getDato();
+		if (a.esHoja()) {
+			if (cont > this.max) {
+				this.max = cont;
+			}	
+		}else {
+			if (a.tieneHijoIzquierdo()) {
+				calcRetardo(cont, a.getHijoIzquierdo());
+			}
+			
+			if (a.tieneHijoDerecho()) {
+				calcRetardo(cont, a.getHijoDerecho());
+			}
+		}
+		
+	}
+	
+}
